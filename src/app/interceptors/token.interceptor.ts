8f90d12299c9d5d17c0facba56fa1ba936f81911
ravi-ashara <tbs.raviashara@gmail.com>
2021-10-29 15:08:06 +0530

    Setup Interceptor With RefreshToken api

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,23 +1,18 @@
+import { CallApiService } from './../services/callApi/call-api.service';
 import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor,
-    HttpResponse
+    HttpInterceptor, HttpHandler, HttpRequest, HttpEvent
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
+import { Observable, throwError } from 'rxjs';
+import { catchError, switchMap, map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    constructor() { }
+    constructor(public callAPI: CallApiService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const token = localStorage.getItem('token');
 
-        if (token) {
+        if (token && !request.url.includes('token')) {
             request = request.clone({
                 setHeaders: {
                     'Authorization': `bearer ${token}`,
@@ -29,11 +24,36 @@ export class TokenInterceptor implements HttpInterceptor {
         }
 
         return next.handle(request).pipe(
-            map((event: HttpEvent<any>) => {
-                if (event instanceof HttpResponse) {
-                    console.log('event--->>>', event);
+            map((res: any) => {
+                if (res.status === 200) {
+                    // console.log(res.body);
                 }
-                return event;
-            }));
+                return res;
+            }),
+            catchError(error => {
+                if (error.status === 401) {
+                    return this.handle401Error(request, next);
+                }
+                let errorMsg = error.error instanceof ErrorEvent ? `Error: ${error.error.message}` : `Error Code: ${error.status},  Message: ${error.message}`;
+                console.log(errorMsg);
+
+                return throwError(errorMsg);
+            })
+        );
     }
+
+    handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+        return this.callAPI.refreshToken().pipe(
+            switchMap((response: any) => {
+                localStorage.token = response.access_token;
+                request = this.addTokenHeader(request, response.access_token);
+                return next.handle(request);
+            })
+        )
+    }
+
+    addTokenHeader(request: HttpRequest<any>, token: string) {
+        return request.clone({ headers: request.headers.set("Authorization", `bearer ${token}`) });
+    }
+
 }
